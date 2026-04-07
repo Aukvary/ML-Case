b81a2a5dd79ac8310c70dcd116803a03943220f1
Ivan Litvin <ivanlitvin51@gmail.com>
2026-04-07 14:18:28 +0700

    add model and main page with fastapi

--- services/ai_model/main.py
+++ services/ai_model/main.py
@@ -1,42 +1,55 @@
-from fastapi import FastAPI
+import os
+from fastapi import FastAPI, Header, HTTPException, Depends
+from dotenv import load_dotenv
 from pydantic import BaseModel
 from src.model import TextEmbedder
 
-app = FastAPI() # API
+load_dotenv()
+INTERNAL_API_KEY = os.getenv("INTERNAL_API_KEY")
+
+app = FastAPI()
 embedder = TextEmbedder() # Model
 
-class VectorRequest(BaseModel): # Фильтрация
+ # Фильтрация
+class VectorRequest(BaseModel):
     type: str
     text: str
 
-# Лехин endpoint
+# Доступ
+async def verify(app_secret: str = Header(None, alias="App-Secret")):
+    if app_secret != INTERNAL_API_KEY:
+        raise HTTPException(status_code=401, detail="Unauthorized: Invalid App-Secret")
+    else:
+        return app_secret
+
+# health-check
 @app.get("/")
 def read_root():
     return {"service": "model", "status": "active"}
 
-# Мой endpoint
+# model-info
+@app.get("/model-info")
+async def get_model_info(authorized: str = Depends(verify)):
+    return {
+        "model_name": "multilingual-e5-small",
+        "vector_dim": embedder.dim
+    }
+
+# 2vec
 @app.post("/vectorize")
 async def vectorize(data: VectorRequest):
     # Запрос
     if data.type == "query":
         vector = embedder.get_embedding(data.text, data.type)
-        return {
-            "vector": vector,
-            "dim": embedder.dim
-        }
+        return {"vector": vector, "dim": embedder.dim}
+    
     # Документ
     else:
         chunks = embedder.split_text(data.text)
         payload = []
         
-        for chunk in chunks: # Обработка чанков
+        for chunk in chunks:
             vector = embedder.get_embedding(chunk, "passage")
-            payload.append({
-                "content": chunk,
-                "vector": vector
-            })
+            payload.append({"content": chunk, "vector": vector})
 
-        return {
-            "type": "passage",
-            "chunks": payload
-        }
+        return {"type": "passage", "chunks": payload}
