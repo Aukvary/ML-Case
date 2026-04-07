c7f471e6a0f408e29915a15f9d1ffa2f9c8aa865
Aukvary <154656953+Aukvary@users.noreply.github.com>
2026-04-07 19:24:33 +0700

    bus api

---
+++ services/api_bus/src/front_api.py
@@ -0,0 +1,17 @@
+import httpx
+import psycopg
+import os
+from fastapi import APIRouter, HTTPException
+from pydantic import BaseModel
+from src.cfg import front_url
+from model_api import file_to_vec
+
+router = APIRouter(prefix="/front", tags=["Front Interaction"])
+
+class FileUploadRequest(BaseModel):
+    url: str
+    filename: str
+
+@router.put("/upload_file")
+async def upload_file(request: FileUploadRequest):
+    pass
